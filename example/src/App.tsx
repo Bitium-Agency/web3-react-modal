1997340f6b54326c76adaba56f876bdc2b6d6b3c
mehrab <mehrab572@gmail.com>
2022-05-30 19:22:46 +0430

    add custom component feature

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -4,7 +4,11 @@ import './App.css'
 import { WalletButton } from 'wallet-button'
 import { useWeb3React, UnsupportedChainIdError } from '@web3-react/core'
 
+
+
 function App() {
+  const ExampleFC :React.FC = () => <button >Custom Component</button>;
+
   const { account, chainId, active } = useWeb3React()
   const WCConfigs = {
     rpc: {
@@ -16,13 +20,78 @@ function App() {
     <div className='App'>
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
-
         <WalletButton
           useWeb3React={useWeb3React}
           walletConnectConfigs={WCConfigs}
           onError={(error) => {
             alert(error)
           }}
+          supportedChains={[
+            {
+              chainId: 1,
+              name: 'Mainnet',
+              rpcUrl:
+                'https://mainnet.infura.io/v3/70d9c70a15ad4cdd91f57979fd0d9e21',
+              nativeCurrency: {
+                name: 'Ether',
+                symbol: 'ETH',
+                decimals: 18,
+              },
+            },
+            {
+              chainId: 3,
+              name: 'Ropsten',
+              rpcUrl:
+                'https://ropsten.infura.io/v3/70d9c70a15ad4cdd91f57979fd0d9e21',
+              nativeCurrency: {
+                name: 'Ether',
+                symbol: 'ETH',
+                decimals: 18,
+              },
+            },
+          ]}
+          Render={<ExampleFC />}
+        /><br/>
+        <WalletButton
+          useWeb3React={useWeb3React}
+          walletConnectConfigs={WCConfigs}
+          onError={(error) => {
+            alert(error)
+          }}
+          supportedChains={[
+            {
+              chainId: 1,
+              name: 'Mainnet',
+              rpcUrl:
+                'https://mainnet.infura.io/v3/70d9c70a15ad4cdd91f57979fd0d9e21',
+              nativeCurrency: {
+                name: 'Ether',
+                symbol: 'ETH',
+                decimals: 18,
+              },
+            },
+            {
+              chainId: 3,
+              name: 'Ropsten',
+              rpcUrl:
+                'https://ropsten.infura.io/v3/70d9c70a15ad4cdd91f57979fd0d9e21',
+              nativeCurrency: {
+                name: 'Ether',
+                symbol: 'ETH',
+                decimals: 18,
+              },
+            },
+          ]}
+        /><br/>
+        <WalletButton
+          useWeb3React={useWeb3React}
+          walletConnectConfigs={WCConfigs}
+          onError={(error) => {
+            alert(error)
+          }}
+          buttonText='CUSTOM TEXT'
+          switchText='CUSTOM SWITCH TEXT'
+          connectText='CUSTOM CONNECT TEXT'
           supportedChains={[
             {
               chainId: 1,
